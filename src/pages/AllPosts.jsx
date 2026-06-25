939f77d7f67cb73ce81fba3e6f3203bac1f81b0c
Kunj Sherathiya <177952870+Kunj65@users.noreply.github.com>
2026-06-25 10:31:02 +0530

    Whole Commit

---
+++ src/pages/AllPosts.jsx
@@ -0,0 +1,36 @@
+import React,{useEffect} from 'react'
+import { Container, PostCard } from '../components'
+import appwriteService from "../appwrite/config";
+
+function AllPosts() {
+    const [posts, setPosts] = React.useState([])
+    
+    useEffect(() => {
+        appwriteService.getPosts([]).then((posts) => {
+            if (posts) setPosts(posts.documents)
+        })
+    }, [])
+
+    // ❌ DELETE THESE LINES BELOW - this is causing infinite loop
+    // appwriteService.getPosts([]).then((posts) => {
+    //     if (posts) {
+    //         setPosts(posts.documents)
+    //     }
+    // })
+
+    return (
+        <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard {...post} />
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default AllPosts
